2a2ddc3b1e7f5e0653100e0af4e5d2891be9c008
kumpelblase2 <tim@infinityblade.de>
2013-10-11 14:11:10 +0200

    Added ability to get the volume of hollow cubes in n dimensions

--- Aufgabe_1/lib/hollow_cube_volume.rb
+++ Aufgabe_1/lib/hollow_cube_volume.rb
@@ -4,20 +4,32 @@ require "ext_elems_v2"
 require "ext_modules_v2"
 require "ext_lists_v2"
 
-# Calculation of hollow cube volume
-# hollow_cube_volume ::= Nat x Nat =>? Nat :: (in_length_outer, in_length_inner) ::::
-# (in_length_outer >= in_length_inner) ::
+# Calculates the volume of a hollow cube with the given amount of dimensions
+# hollow_cube_volume ::= Nat x Nat x Nat =>? Nat :: (in_length_outer, in_length_inner, in_dimensions) ::::
+# (in_length_outer >= in_length_inner) :: (in_dimensions >= 0) ::
 # Test (3, 3) => 0, (3,2) => 19,
 # (1,2) => Err, ("", 4) => Err
-
-def hollow_cube_volume(in_length_outer, in_length_inner)
-  if(in_length_outer < in_length_inner)
-    raise("a is not >= b")
-  elsif(in_length_outer < 0 or in_length_inner < 0)
-    raise("either a or b < 0")
-  end
-
-  volume_a = in_length_outer * in_length_outer * in_length_outer
-  volume_b = in_length_inner * in_length_inner * in_length_inner
+# (3, 3, 0) => 0, (4, 3, 4) => 175
+# (1, 1, -1) => Err, (-1, 1, 2) => Err
+def hollow_cube_volume(in_length_outer, in_length_inner, in_dimensions = 3)
+  check_pre(((in_length_outer.nat?) and 
+				(in_length_inner.nat?) and 
+				(in_dimensions.nat?) and
+				(in_length_outer >= in_length_inner)))
+  volume_a = cube_volume(in_length_outer, in_dimensions)
+  volume_b = cube_volume(in_length_inner, in_dimensions)
   volume_a - volume_b
+end
+
+# Calculates the volume of a cube in given dimensions
+# By default, the dimensions are 3
+# cube_volume ::= Nat x Nat => Nat :: (in_length, in_dimensions) ::::
+# (in_length >= 0) :: (in_dumensions >= 0) ::
+def cube_volume(in_length, in_dimensions = 3)
+	check_pre(((in_length.nat?) and (in_dimensions.nat?)))
+	if in_dimensions == 0 then
+		return 0
+	end
+	
+	return in_length ** in_dimensions
 end
