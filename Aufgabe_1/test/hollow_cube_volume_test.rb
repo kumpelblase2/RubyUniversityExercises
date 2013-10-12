e96b75a61f3b00f65de78e0110b63c9a3885e05f
Tim H <admin@infinityblade.de>
2013-10-12 17:00:46 +0200

    Formatting seemed wrong.

--- Aufgabe_1/test/hollow_cube_volume_test.rb
+++ Aufgabe_1/test/hollow_cube_volume_test.rb
@@ -15,18 +15,18 @@ class HollowCubeVolumeTest < Test::Unit::TestCase
   end
   
   def test_hollow_cube_multi_dimension
-	assert_equal(0, hollow_cube_volume(3, 3, 0))
-	assert_equal(hollow_cube_volume(3, 3), hollow_cube_volume(3, 3, 3))
-	assert_equal(256 - 81, hollow_cube_volume(4, 3, 4))
-	assert_raise(RuntimeError) { |i| hollow_cube_volume(-1, 1, 0)}
+	  assert_equal(0, hollow_cube_volume(3, 3, 0))
+	  assert_equal(hollow_cube_volume(3, 3), hollow_cube_volume(3, 3, 3))
+	  assert_equal(175, hollow_cube_volume(4, 3, 4))
+	  assert_raise(RuntimeError) { |i| hollow_cube_volume(-1, 1, 0) }
   end
   
   def test_cube_volume
-	assert_equal(0, cube_volume(0))
-	assert_equal(0, cube_volume(3, 0))
-	assert_equal(27, cube_volume(3))
-	assert_equal(8, cube_volume(2))
-	assert_equal(4, cube_volume(2, 2))
-	assert_raise(RuntimeError) { |i| cube_volume(-1)}
+	  assert_equal(0, cube_volume(0))
+	  assert_equal(0, cube_volume(3, 0))
+	  assert_equal(27, cube_volume(3))
+	  assert_equal(8, cube_volume(2))
+	  assert_equal(4, cube_volume(2, 2))
+	  assert_raise(RuntimeError) { |i| cube_volume(-1) }
   end
 end
