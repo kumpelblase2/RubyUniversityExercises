770314535cb18653419f5d616c07150305740a6d
Tim H <admin@infinityblade.de>
2013-11-22 21:12:22 +0100

    Add changes from university.

--- Aufgabe_3/lib/graphics.rb
+++ Aufgabe_3/lib/graphics.rb
@@ -72,85 +72,36 @@ end
 
 def shape_include?(in_shape, in_point)
 	check_pre(((point?(in_point)) and (shape?(in_shape))))
-	# (shape1d?(in_shape) ? shape1d_include?(in_shape, in_point) : shape2d_include?(in_shape, in_point))
-	#(union_shape?(in_shape) ? union_include?(in_shape, in_point) : (shape1d?(in_shape) ? shape1d_include(in_shape, in_point) : shape2d_include?(in_shape, in_point)))
-	#(shape1d?(in_shape) ? shape1d_include?(in_shape, in_point) : shape2d_include?(in_shape, in_point))
-	(union_shape?(in_shape) ? union_include?(in_shape, in_point) : (shape1d?(in_shape) ? shape1d_include?(in_shape, in_point) : shape2d_include?(in_shape, in_point)))
-end
-
-def union_include?(in_union, in_point)
-	shape_include?(in_union.left, in_point) or shape_include?(in_union.right, in_point)
-end
-
-def union1d_include?(in_union, in_point)
-	shape1d_include?(in_union.left, in_point) or shape1d_include?(in_union.right, in_point)
-end
-
-def union2d_include?(in_union, in_point)
-	shape2d_include?(in_union.left, in_point) or shape2d_include?(in_union.right, in_point)
-end
-
-def shape1d_include?(in_shape, in_point)
-	check_pre((shape1d?(in_shape) and point1d?(in_point)))
-	#(in_shape.range1d? ? range1d_include?(in_shape, in_point) : point1d_include?(in_shape, in_point))
-	(in_shape.union1d? ? union1d_include?(in_shape, in_point) : range1d_include?(in_shape, in_point))
-end
-
-def shape2d_include?(in_shape, in_point)
-	(in_shape.union2d? ? union2d_include?(in_shape, in_point) : range2d_include?(in_shape, in_point))
-end
-
-def point1d_include?(in_point1, in_point2)
-	in_point1 == in_point2
+	if union_shape?(in_shape) then shape_include?(in_shape.left, in_point) or shape_include?(in_shape.right, in_point)
+  elsif in_shape.range1d? then range1d_include?(in_shape, in_point)
+  elsif in_shape.range2d? then shape_include?(in_shape.x_range, in_point.x) and shape_include?(in_shape.y_range, in_point.y)
+  elsif check_pre(false)
+  end
 end
 
 def range1d_include?(in_range, in_point)
 	(in_range.first..in_range.last).include?(in_point)
-	#in_point.in?(in_range.first..in_range.last)
-end
-
-def point2d_include?(in_point1, in_point2)
-	(in_point1.x == in_point2.x) and (in_point1.y == in_point2.y)
-end
-
-def range2d_include?(in_range, in_point)
-	range1d_include?(in_range.x_range, in_point.x) and range1d_include?(in_range.y_range, in_point.y)
 end
 
 def translate(in_shape, in_point)
-	check_pre(((shape?(in_shape)) or (point?(in_point))))
-	(shape1d?(in_shape) ? translate1d(in_shape, in_point) : translate2d(in_shape, in_point))
-end
-
-def translate1d(in_shape, in_point)
-	check_pre((shape1d?(in_shape) and point1d?(in_point)))
-	(union_shape?(in_shape) ? Union1d[translate1d(in_shape.left, in_point), translate1d(in_shape.right, in_point)] : translate_range1d(in_shape, in_point))
-end
-
-def translate_range1d(in_range, in_point)
-	Range1d[translate_point(in_range.first, in_point), translate_point(in_range.last, in_point)]
+	check_pre(((shape?(in_shape)) and (point?(in_point))))
+  if in_shape.union1d? then Union1d[translate(in_shape.left, in_point), translate(in_shape.right, in_point)]
+  elsif in_shape.union2d? then Union2d[translate(in_shape.left, in_point), translate(in_shape.right, in_point)]
+  elsif in_shape.range2d? then Range2d[translate(in_shape.x_range, in_point.x), translate(in_shape.y_range, in_point.y)]
+  else Range1d[translate_point(in_shape.first, in_point), translate_point(in_shape.last, in_point)]
+  end
 end
 
 def translate_point(in_point, in_point_trans)
 	in_point + in_point_trans
 end
 
-def translate2d(in_shape, in_point)
-	check_pre(((shape2d?(in_shape)) and (in_point.point2d?)))
-	(union_shape?(in_shape) ? Union2d[translate2d(in_shape.left, in_point), translate2d(in_shape.right, in_point)] : translate_range2d(in_shape, in_point))
-end
-
-def translate_range2d(in_range, in_point)
-	Range2d[translate_range1d(in_range.x_range, in_point.x), translate_range1d(in_range.y_range, in_point.y)]
-end
-
 def bounds(in_shape)
 	check_pre((shape?(in_shape)))
-	(shape1d?(in_shape) ? bounds1d(in_shape) : bounds2d(in_shape))
-end
-
-def bounds1d(in_shape)
-	in_shape.range1d? ? in_shape : bounding_range(bounds1d(in_shape.left), bounds1d(in_shape.right))
+  if (in_shape.range1d? or in_shape.range2d?) then in_shape
+  elsif (in_shape.union1d? or in_shape.union2d?) then bounding_range(bounds(in_shape.left), bounds(in_shape.right))
+  else check_pre(false)
+  end
 end
 
 def combine_range(in_range1, in_range2)
@@ -165,10 +116,6 @@ def smallest(in_first, in_second)
 	(in_first < in_second ? in_first : in_second)
 end
 
-def bounds2d(in_shape)
-	in_shape.range2d? ? in_shape : bounding_range(bounds2d(in_shape.left), bounds2d(in_shape.right))
-end
-
 def combine_range2d(in_range, in_range2)
 	Range2d[combine_range(in_range.x_range, in_range2.x_range), combine_range(in_range.y_range, in_range2.y_range)]
 end
@@ -178,6 +125,7 @@ def bounding_range(in_range, in_range2)
 end
 
 def equal_by_dim?(in_obj, in_obj2)
+  check_pre((graph_obj?(in_obj) and graph_obj?(in_obj2)))
 	get_dim(in_obj) == get_dim(in_obj2)
 end
 
@@ -186,36 +134,46 @@ def get_dim(in_obj)
 end
 
 def equal_by_tree?(in_obj1, in_obj2)
-	(equal_by_dim?(in_obj1, in_obj2) ? equal(in_obj1, in_obj2) : false)
+  check_pre((equal_by_dim?(in_obj1, in_obj2)))
+  if union_shape?(in_obj1) then (union_shape?(in_obj2) and equal_union?(in_obj1, in_obj2))
+  elsif in_obj1.range1d? then (in_obj2.range1d? and equal_range?(in_obj1, in_obj2))
+  elsif in_obj1.range2d? then (in_obj2.range2d? and equal_by_tree?(in_obj1.x_range, in_obj2.x_range) and equal_by_tree?(in_obj1.y_range, in_obj2.y_range))
+  else false
+  end
 end
 
-def equal(in_obj1, in_obj2)
-	if union_shape?(in_obj1) then
-		(union_shape?(in_obj2) and equal(in_obj1.left, in_obj2.left) and equal(in_obj1.right, in_obj2.right))
-	else
-		((not union_shape?(in_obj2)) and ((point?(in_obj1) and point?(in_obj1)) or (prim_shape?(in_obj1) and prim_shape?(in_obj2))))
-	end
+def equal_union?(in_obj1, in_obj2)
+  ((equal_by_tree?(in_obj1.left, in_obj2.left) and equal_by_tree?(in_obj1.right, in_obj2.right)) or (equal_by_tree?(in_obj1.left, in_obj2.right) and equal_by_tree?(in_obj1.right, in_obj2.left)))
+end
+
+def equal_range?(in_range1, in_range2)
+  (in_range1.first == in_range2.first) and (in_range1.last == in_range2.last)
 end
 
 def equal_by_trans?(in_obj1, in_obj2)
-	check_pre((equal_by_tree?(in_obj1, in_obj2)))
-	(union_shape?(in_obj1) ? (equal_by_trans?(in_obj1.left, in_obj2.left) and equal_by_trans?(in_obj1.right, in_obj2.right)) : are_multiple?(in_obj1, in_obj2))
+  check_pre((equal_by_dim?(in_obj1, in_obj2)))
+  translation = get_translation_point(bounds(in_obj1), bounds(in_obj2))
+  if translation == nil then false
+  else equal_by_points?(translate(in_obj1, translation), in_obj2)
+  end
 end
 
-def are_multiple?(in_range1, in_range2)
-	if shape1d?(in_range1) then
-		get_trans_num(in_range1, in_range2) != nil
-	else
-		(are_multiple?(in_range1.x_range, in_range2.x_range) and are_multiple?(in_range1.y_range, in_range2.y_range))
-	end
+def get_translation_point(in_range1, in_range2)
+  if in_range1.range1d? then get_trans_num(in_range1, in_range2)
+  elsif in_range1.range2d? then
+    x = get_translation_point(in_range1.x_range, in_range2.x_range)
+    y = get_translation_point(in_range1.y_range, in_range2.y_range)
+    if x == nil or y == nil then nil
+    else Point2d[x, y]
+    end
+  else check_pre(false)
+  end
 end
 
 def get_trans_num(in_range1, in_range2)
-	first = in_range1.first / in_range2.first
-	second = in_range1.last / in_range2.last
-	bigger = biggest(first, second)
-	smaller = smallest(first, second)
-	(bigger % smaller == 0 ? bigger : nil)
+	first = in_range1.first - in_range2.first
+	second = in_range1.last - in_range2.last
+	(first == second ? first : nil)
 end
 
 def equal_by_points?(in_obj1, in_obj2)
