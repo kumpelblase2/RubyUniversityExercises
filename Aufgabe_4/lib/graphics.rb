0208005aafaf726112f93d2f6fa9d5f3ea3d8faa
kumpelblase2 <tim@infinityblade.de>
2013-12-19 18:00:43 +0100

    Add Diff shapes to exercise 4.
    Also implement substraction for points

--- Aufgabe_4/lib/graphics.rb
+++ Aufgabe_4/lib/graphics.rb
@@ -13,12 +13,15 @@ class Object
 	def range2d?() false end
 	def union1d?() false end
 	def union2d?() false end
+	def diff1d?() false end
+	def diff2d?() false end
 	def shape?() false end
 	def point?() false end
 	def prim_shape?() false end
 	def union_shape?() false end
 	def comp_shape?() false end
 	def graph_obj?() false end
+	def diff_shape?() false end
 end
 
 class GraphObj
@@ -45,6 +48,11 @@ class Shape1d < Shape
 		check_pre((in_obj.shape1d?))
 		Union1d[self, in_obj]
 	end
+	
+	def -(in_obj)
+		check_pre((in_obj.shape1d?))
+		Diff1d[self, in_obj]
+	end
 end
 
 class Shape2d < Shape
@@ -53,6 +61,11 @@ class Shape2d < Shape
 	def +(in_obj)
 		check_pre((in_obj.shape2d?))
 		Union2d[self, in_obj]
+	end
+	
+	def -(in_obj)
+		check_pre((in_obj.shape2d?))
+		Diff2d[self, in_obj]
 	end
 end
 
@@ -87,7 +100,13 @@ class Point1d < Point
 	end
 	
 	def +(in_obj)
+		check_pre((in_obj.point1d?))
 		self.translate(in_obj)
+	end
+	
+	def -(in_obj)
+		check_pre((in_obj.point1d?))
+		self.translate(Point1d[-(in_obj.point)])
 	end
 end
 
@@ -217,7 +236,13 @@ class Point2d < Point
 	end
 	
 	def +(in_obj)
+		check_pre((in_obj.point2d?))
 		self.translate(in_obj)
+	end
+	
+	def -(in_obj)
+		check_pre((in_obj.point2d?))
+		self.translate(Point2d[-(in_obj.x), -(in_obj.y)])
 	end
 end
 
@@ -316,32 +341,6 @@ class Union2d < Shape2d
 	end
 end
 
-#def shape_include?(in_shape, in_point)
-#	check_pre(((point?(in_point)) and (shape?(in_shape))))
-#	if union_shape?(in_shape) then shape_include?(in_shape.left, in_point) or shape_include?(in_shape.right, in_point)
-#  elsif in_shape.range1d? then range1d_include?(in_shape, in_point)
-#  elsif in_shape.range2d? then shape_include?(in_shape.x_range, in_point.x) and shape_include?(in_shape.y_range, in_point.y)
-#  elsif check_pre(false)
-#  end
-#end
-
-#def range1d_include?(in_range, in_point)
-#	(in_range.first..in_range.last).include?(in_point)
-#end
-
-#def translate(in_shape, in_point)
-#	check_pre(((shape?(in_shape)) and (point?(in_point))))
-#  if in_shape.union1d? then Union1d[translate(in_shape.left, in_point), translate(in_shape.right, in_point)]
-#  elsif in_shape.union2d? then Union2d[translate(in_shape.left, in_point), translate(in_shape.right, in_point)]
-#  elsif in_shape.range2d? then Range2d[translate(in_shape.x_range, in_point.x), translate(in_shape.y_range, in_point.y)]
-#  else Range1d[translate_point(in_shape.first, in_point), translate_point(in_shape.last, in_point)]
-#  end
-#end
-#
-#def translate_point(in_point, in_point_trans)
-#	in_point + in_point_trans
-#end
-
 def combine_range(in_range1, in_range2)
 	Range1d[smallest(in_range1.first, in_range2.first),biggest(in_range1.last, in_range2.last)]
 end
@@ -362,58 +361,87 @@ def bounding_range(in_range, in_range2)
 	in_range.range2d? ? combine_range2d(in_range, in_range2) : combine_range(in_range, in_range2)
 end
 
-#def equal_by_dim?(in_obj, in_obj2)
-#  check_pre((graph_obj?(in_obj) and graph_obj?(in_obj2)))
-#	get_dim(in_obj) == get_dim(in_obj2)
-#end
-#
-#def get_dim(in_obj)
-#	((shape1d?(in_obj) or point1d?(in_obj)) ? 1 : 2)
-#end
-
-#def equal_by_tree?(in_obj1, in_obj2)
-#  check_pre((equal_by_dim?(in_obj1, in_obj2)))
-#  if union_shape?(in_obj1) then (union_shape?(in_obj2) and equal_union?(in_obj1, in_obj2))
-#  elsif in_obj1.range1d? then (in_obj2.range1d? and equal_range?(in_obj1, in_obj2))
-#  elsif in_obj1.range2d? then (in_obj2.range2d? and equal_by_tree?(in_obj1.x_range, in_obj2.x_range) and equal_by_tree?(in_obj1.y_range, in_obj2.y_range))
-#  else false
-#  end
-#end
-#
-#def equal_union?(in_obj1, in_obj2)
-#  ((equal_by_tree?(in_obj1.left, in_obj2.left) and equal_by_tree?(in_obj1.right, in_obj2.right)) or (equal_by_tree?(in_obj1.left, in_obj2.right) and equal_by_tree?(in_obj1.right, in_obj2.left)))
-#end
-#
-#def equal_range?(in_range1, in_range2)
-#  (in_range1.first == in_range2.first) and (in_range1.last == in_range2.last)
-#end
-
-#def equal_by_trans?(in_obj1, in_obj2)
-#  check_pre((equal_by_dim?(in_obj1, in_obj2)))
-#  translation = get_translation_point(bounds(in_obj1), bounds(in_obj2))
-#  if translation == nil then false
-#  else equal_by_points?(translate(in_obj1, translation), in_obj2)
-#  end
-#end
-
-#def get_translation_point(in_range1, in_range2)
-#  if in_range1.range1d? then get_trans_num(in_range1, in_range2)
-#  elsif in_range1.range2d? then
-#    x = get_translation_point(in_range1.x_range, in_range2.x_range)
-#    y = get_translation_point(in_range1.y_range, in_range2.y_range)
-#    if x == nil or y == nil then nil
-#    else Point2d[x, y]
-#    end
-#  else check_pre(false)
-#  end
-#end
-#
-#def get_trans_num(in_range1, in_range2)
-#	first = in_range1.first - in_range2.first
-#	second = in_range1.last - in_range2.last
-#	(first == second ? first : nil)
-#end
-
 def equal_by_points?(in_obj1, in_obj2)
 
+end
+
+
+class Diff1d < Shape1d
+	def diif1d?() true end
+	def diff_shape?() true end
+	
+	def initialize(in_main, in_substract)
+		@main = in_main
+		@substract = in_substract
+	end
+	
+	def invariant?() self.main.shape1d? and self.substract.shape1d? end
+	def main() @main end
+	def substract() @substract end
+	
+	def translate(in_point)
+		check_pre((in_point.point1d?))
+		Diff1d[self.main.translate(in_point), self.substract.translate(in_point)]
+	end
+	
+	def same_tree?(in_obj)
+		in_obj.diff1d? and self.main.same_tree?(in_obj.main) and self.substract.same_tree?(in_obj.substract)
+	end
+	
+	def bounds()
+		self.main.bounds()
+	end
+	
+	def get_translation_to(in_obj)
+		check_pre((in_obj.graph_obj?))
+		self.bounds().get_translation_to(in_obj.bounds())
+	end
+	
+	def ==(in_obj)
+		self.equal?(in_obj) or (in_obj.diff1d? and self.main == in_obj.main and self.substract == in_obj.substract)
+	end
+	
+	def to_s()
+		"Diff1d[#{self.main.to_s}, #{self.substract.to_s}]"
+	end
+end
+
+class Diff2d < Shape2d
+	def diif2d?() true end
+	def diff_shape?() true end
+	
+	def initialize(in_main, in_substract)
+		@main = in_main
+		@substract = in_substract
+	end
+	
+	def invariant?() self.main.shape2d? and self.substract.shape2d? end
+	def main() @main end
+	def substract() @substract end
+	
+	def translate(in_point)
+		check_pre((in_point.point2d?))
+		Diff2d[self.main.translate(in_point), self.substract.translate(in_point)]
+	end
+	
+	def same_tree?(in_obj)
+		in_obj.diff2d? and self.main.same_tree?(in_obj.main) and self.substract.same_tree?(in_obj.substract)
+	end
+	
+	def bounds()
+		self.main.bounds()
+	end
+	
+	def get_translation_to(in_obj)
+		check_pre((in_obj.graph_obj?))
+		self.bounds().get_translation_to(in_obj.bounds())
+	end
+	
+	def ==(in_obj)
+		self.equal?(in_obj) or (in_obj.diff2d? and self.main == in_obj.main and self.substract == in_obj.substract)
+	end
+	
+	def to_s()
+		"Diff2d[#{self.main.to_s}, #{self.substract.to_s}]"
+	end
 end
